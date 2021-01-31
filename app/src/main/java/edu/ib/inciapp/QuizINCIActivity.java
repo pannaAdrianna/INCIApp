35cb0fcdc002e5b36ea0110694a6e953605cb801
pannaAdrianna <250319@student.pwr.edu.pl>
2021-01-31 16:28:46 +0100

    cpmmit buttons works

--- app/src/main/java/edu/ib/inciapp/QuizINCIActivity.java
+++ app/src/main/java/edu/ib/inciapp/QuizINCIActivity.java
@@ -3,16 +3,18 @@ package edu.ib.inciapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 public class QuizINCIActivity extends AppCompatActivity {
 
-    private Button btnTrue;
-    private Button btnFalse;
+    Button btnTrue;
+    Button btnFalse;
+    Button btnNext;
     private CardView card;
 
     private Flashcard[] deckOfFlashcards;
@@ -21,24 +23,29 @@ public class QuizINCIActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_i_n_c_i);
-
-
-    }
-
-
-    public void onBtnFalseClick(View view) {
-
-    }
-
-    public void onBtnTrueClick(View view) {
-    }
-
-    public void onCardCLick(View view) {
+        btnTrue = (Button) findViewById(R.id.btnTrue);
+        btnFalse = (Button) findViewById(R.id.btnFalse);
+        btnNext = (Button) findViewById(R.id.btnNext);
+        btnNext.setVisibility(View.INVISIBLE);
         try {
-            card.setCardBackgroundColor(Color.BLUE);
-        }catch (Exception e){
+            btnTrue.setOnClickListener(v -> {
+                btnNext.setVisibility(View.VISIBLE);
+            });
+            btnFalse.setOnClickListener(v -> {
+                btnNext.setVisibility(View.VISIBLE);
+            });
+            btnNext.setOnClickListener(v -> {
+                Toast.makeText(this, "Next Question Please", LENGTH_LONG).show();
+            });
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
+
+    }
+
+
+    public void onBtnNextClick(View view) {
 
     }
 }
